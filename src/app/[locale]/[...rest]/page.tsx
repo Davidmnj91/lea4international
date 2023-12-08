9e8c2df08df8764409a2fb40bac763dca4f74443
DavidMnj91 <david.manja91@gmail.com>
2023-12-08 17:31:08 +0100

    feat: not found page

--- src/app/[locale]/[...rest]/page.tsx
+++ src/app/[locale]/[...rest]/page.tsx
@@ -1,5 +1,20 @@
-import { notFound } from 'next/navigation';
+import Image from 'next/image';
+import not_found from '../../../../public/not_found.png';
+import { buttonTypes } from '@/components/button/button';
+import { useTranslations } from 'next-intl';
+import Link from 'next/link';
 
 export default function CatchAllPage() {
-  notFound();
+  const t = useTranslations('not-found-page');
+
+  return (
+    <div className='flex h-screen flex-col items-center justify-center'>
+      <div>
+        <Image src={not_found.src} alt='404' width={750} height={532} />
+        <Link href={'/'} className={buttonTypes({ intent: 'primary' })}>
+          {t('back-to-home')}
+        </Link>
+      </div>
+    </div>
+  );
 }
