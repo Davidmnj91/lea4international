aac655a74ff554debeff100e0097e9f1d5dae816
DavidMnj91 <david.manja91@gmail.com>
2024-03-24 21:33:29 +0100

    feat: remove european standards logo

--- src/components/partners/partners.tsx
+++ src/components/partners/partners.tsx
@@ -1,7 +1,6 @@
 import { useTranslations } from 'next-intl';
 import { Typography } from '@/components/typography/typography';
 import Image from 'next/image';
-import erasmus_plus_logo from '../../../public/images/partners/erasmus_plus_logo.webp';
 import impronta_logo from '../../../public/images/partners/impronta_logo.webp';
 import nixedonia_logo from '../../../public/images/partners/nixedonia_logo.webp';
 import my_abroad_ally_logo from '../../../public/images/partners/my_abroad_ally_logo.webp';
@@ -15,13 +14,6 @@ export const Partners = () => {
         {t('title')}
       </Typography>
       <div className='flex w-full flex-wrap items-center justify-center gap-14'>
-        <Image
-          src={erasmus_plus_logo.src}
-          alt={'Erasmus+'}
-          width={176}
-          height={176}
-          style={{ width: '176px', height: 'auto' }}
-        />
         <Image
           src={impronta_logo.src}
           alt={'Impronta'}
